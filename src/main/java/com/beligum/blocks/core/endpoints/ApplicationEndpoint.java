ebbdf416e7c5a1e697e342ba75b131a9a1d1f166
Bastiaan Maertens <j@beligum.com>
2014-10-06 10:13:07 +0200

    Initial commit

---
+++ src/main/java/com/beligum/blocks/core/endpoints/ApplicationEndpoint.java
@@ -0,0 +1,21 @@
+package com.beligum.blocks.core.endpoints;
+
+import com.beligum.core.framework.base.R;
+import com.beligum.core.framework.templating.ifaces.Template;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class ApplicationEndpoint
+{
+    @Path("/")
+    @GET
+    public Response index()
+    {
+        Template indexTemplate = R.templateEngine().getEmptyTemplate("/views/index.html");
+
+        return Response.ok(indexTemplate.render()).build();
+    }
+}
