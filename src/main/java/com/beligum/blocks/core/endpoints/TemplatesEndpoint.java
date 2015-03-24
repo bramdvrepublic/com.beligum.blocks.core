f7b2aca0216eb1894a2a60abbeba27764943d6ac
Bram Biesbrouck <b@beligum.com>
2015-03-24 02:13:37 +0100

    Switched to webjars for the libraries and universal constants

--- src/main/java/com/beligum/blocks/core/endpoints/TemplatesEndpoint.java
+++ src/main/java/com/beligum/blocks/core/endpoints/TemplatesEndpoint.java
@@ -1,7 +1,9 @@
 package com.beligum.blocks.core.endpoints;
 
 import com.beligum.core.framework.annotations.JavascriptPackage;
+import com.beligum.core.framework.base.R;
 import com.beligum.core.framework.endpoints.AssetsEndpoint;
+import com.beligum.core.framework.resources.ResourceDescriptor;
 import org.apache.commons.io.FilenameUtils;
 
 import javax.ws.rs.GET;
@@ -26,6 +28,9 @@ public class TemplatesEndpoint extends AssetsEndpoint
     {
         //SECURITY (both the prefix and the normalize)
         String resourcePath = "/templates/" + FilenameUtils.normalize(name);
-        return loadResource(resourcePath);
+
+        ResourceDescriptor resource = R.resourceLoader().getResource(resourcePath, true);
+
+        return loadResource(resource);
     }
 }
