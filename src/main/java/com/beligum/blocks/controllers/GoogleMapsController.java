511a74705a813cb4d4bd10df0b5e2a920d248cca
Wouter Vanhoutte <w@beligum.com>
2015-09-02 13:12:32 +0200

    Add google maps block

---
+++ src/main/java/com/beligum/blocks/controllers/GoogleMapsController.java
@@ -0,0 +1,26 @@
+package com.beligum.blocks.controllers;
+
+import com.beligum.base.server.R;
+import com.beligum.blocks.config.BlocksConfig;
+import com.beligum.blocks.templating.blocks.DefaultTemplateController;
+
+/**
+ * Created by wouter on 2/09/15.
+ */
+public class GoogleMapsController extends DefaultTemplateController
+{
+
+    @Override
+    public void created()
+    {
+
+    }
+
+    public String getApiKey() {
+        String retVal = R.configuration().getString("blocks.core.google-maps.api-key");
+        if (retVal == null) {
+            retVal = "";
+        }
+        return retVal;
+    }
+}
---
+++ src/main/resources/assets/scripts/imports/blocks-google-maps.js
@@ -0,0 +1,45 @@
+/**
+ * Created by wouter on 1/09/15.
+ */
+base.plugin("blocks.imports.GoogleMaps", ["base.core.Class", "blocks.imports.Block", "constants.blocks.core", "messages.blocks.core", "blocks.core.Sidebar", "blocks.core.SidebarUtils", function (Class, Block, BlocksConstants, BlocksMessages, Sidebar, SidebarUtils)
+{
+    var BlocksMaps = this;
+    this.TAGS = ["blocks-google-maps"];
+
+    (this.Class = Class.create(Block.Class, {
+
+        //-----VARIABLES-----
+
+        //-----CONSTRUCTORS-----
+        constructor: function ()
+        {
+            BlocksMaps.Class.Super.call(this);
+        },
+
+        //-----IMPLEMENTED METHODS-----
+        init: function ()
+        {
+        },
+        focus: function (block, element, hotspot, event)
+        {
+            BlocksMaps.Class.Super.prototype.focus.call(this, block, element, hotspot, event);
+        },
+        blur: function (block, element)
+        {
+            BlocksMaps.Class.Super.prototype.blur.call(this, block, element);
+        },
+        getConfigs: function (block, element)
+        {
+            var retVal = BlocksMaps.Class.Super.prototype.getConfigs.call(this, block, element);
+
+            return retVal;
+        },
+        getWindowName: function ()
+        {
+            return BlocksMessages.widgetSpacerTitle;
+        }
+
+        //-----PRIVATE METHODS-----
+
+    })).register(this.TAGS);
+}]);
