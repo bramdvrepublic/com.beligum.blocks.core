3035203ae64e78fa28d69ca7b3a1fcd2ccac20b0
Bram Biesbrouck <b@beligum.com>
2016-09-05 15:22:27 +0200

    Release 0.2.16
    - Moved search type selection to results list

--- src/main/resources/assets/scripts/imports/blocks-search/box.js
+++ src/main/resources/assets/scripts/imports/blocks-search/box.js
@@ -32,25 +32,6 @@ base.plugin("mot.blocks.search", ["base.core.Class", "blocks.imports.Block", "co
         {
             var retVal = SearchBox.Class.Super.prototype.getConfigs.call(this, block, element);
 
-            var VAL_PROP = "curieName";
-            retVal.push(this.addUniqueAttributeValueAsync(Sidebar, block.element, BlocksMessages.searchBoxType, BlocksConstants.SEARCH_BOX_TYPE_ARG, "/blocks/admin/rdf/search/classes/", "title", VAL_PROP,
-                null
-                // function changeListener(oldValueTerm, newValueTerm)
-                // {
-                //     if (oldValueTerm) Logger.info(oldValueTerm[VAL_PROP]);
-                //     if (newValueTerm) Logger.info(newValueTerm[VAL_PROP]);
-                //
-                //     //for now, we don't allow the combobox to switch to an "empty" value, so ignore if that happens (probably during initialization)
-                //     if (!newValueTerm) {
-                //         return;
-                //     }
-                //
-                //     // don't change anything if they're both the same
-                //     if (oldValueTerm && oldValueTerm[VAL_PROP] == newValueTerm[VAL_PROP]) {
-                //         return;
-                //     }
-                // }
-            ));
             retVal.push(this.addOptionalClass(Sidebar, block.element, BlocksMessages.searchBoxInline, BlocksConstants.SEARCH_BOX_CLASS_INLINE));
 
             return retVal;
--- src/main/resources/assets/scripts/imports/blocks-search/results.js
+++ src/main/resources/assets/scripts/imports/blocks-search/results.js
@@ -32,6 +32,26 @@ base.plugin("mot.blocks.results", ["base.core.Class", "blocks.imports.Block", "c
         {
             var retVal = SearchResults.Class.Super.prototype.getConfigs.call(this, block, element);
 
+            var VAL_PROP = "curieName";
+            retVal.push(this.addUniqueAttributeValueAsync(Sidebar, block.element, BlocksMessages.searchBoxType, BlocksConstants.SEARCH_BOX_TYPE_ARG, "/blocks/admin/rdf/search/classes/", "title", VAL_PROP,
+                null
+                // function changeListener(oldValueTerm, newValueTerm)
+                // {
+                //     if (oldValueTerm) Logger.info(oldValueTerm[VAL_PROP]);
+                //     if (newValueTerm) Logger.info(newValueTerm[VAL_PROP]);
+                //
+                //     //for now, we don't allow the combobox to switch to an "empty" value, so ignore if that happens (probably during initialization)
+                //     if (!newValueTerm) {
+                //         return;
+                //     }
+                //
+                //     // don't change anything if they're both the same
+                //     if (oldValueTerm && oldValueTerm[VAL_PROP] == newValueTerm[VAL_PROP]) {
+                //         return;
+                //     }
+                // }
+            ));
+
             retVal.push(this.addUniqueAttributeValue(Sidebar, block.element, BlocksMessages.searchBoxResultsFormat, BlocksConstants.SEARCH_RESULTS_FORMAT_ARG,
                 [
                     {
