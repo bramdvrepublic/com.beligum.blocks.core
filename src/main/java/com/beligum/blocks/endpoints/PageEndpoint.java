7144d70754224bb99a50e141edd66ef89c22ccd1
Bram Biesbrouck <b@beligum.com>
2016-09-07 15:14:02 +0200

    Release 0.2.19
    - Re-enabled transactions during indexing

--- src/main/java/com/beligum/blocks/endpoints/PageEndpoint.java
+++ src/main/java/com/beligum/blocks/endpoints/PageEndpoint.java
@@ -513,7 +513,7 @@ public class PageEndpoint
                             completed = true;
                         }
                         finally {
-                            //see above
+                            //see indexClear()
                             StorageFactory.releaseCurrentRequestTx(false);
                         }
                     }
--- src/main/java/com/beligum/blocks/fs/index/SesamePageIndexerConnection.java
+++ src/main/java/com/beligum/blocks/fs/index/SesamePageIndexerConnection.java
@@ -380,14 +380,8 @@ public class SesamePageIndexerConnection extends AbstractIndexConnection impleme
     {
         //only need to do it once (at the beginnign of a method using a tx)
         if (!this.registeredTransaction) {
-            //Note: this check allows us to NOT join (expensive) a transaction
-            // when it's not strictly necessary. It also means that this indexation
-            // will not be transactional if no previous transaction was already started!
-            if (StorageFactory.hasCurrentRequestTx()) {
-
-                //attach this connection to the transaction manager
-                StorageFactory.getCurrentRequestTx().registerResource(this);
-            }
+            //attach this connection to the transaction manager
+            StorageFactory.getCurrentRequestTx().registerResource(this);
             this.registeredTransaction = true;
         }
     }
